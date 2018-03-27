8c6c3c0292df826bf6cd83f9b4299445ce774b5f
seekworser <seek_worser@yahoo.co.jp>
2018-03-27 17:02:55 +0900

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sttomog",
+    version="0.1",
+    url="https://github.com/seekworser/sttomog",
+    author="Shunsuke Fujisawa",
+    author_email="seek_worser@yahoo.co.jp",
+    description="Manage quantum state tomography",
+    packages=find_packages()
+)
